e133a3441bb341071b1be9b0dad81dcc2163570b
Lauriane Anthony <lauriane.anthony@talkspirit.com>
2015-03-24 14:37:01 +0100

    change name of save and get function

--- app/scripts/lib/localstorage.js
+++ app/scripts/lib/localstorage.js
@@ -1,11 +1,11 @@
 'use strict';
 
-function getLocalStorage(key, localStorageService){
+function getData(key, localStorageService){
     var projectsInStore = localStorageService.get(key);
     return projectsInStore || [];
 }
 
-function saveLocalStorage(key, value, localStorageService){
+function saveData(key, value, localStorageService){
   localStorage.clear();
   localStorageService.set(key, value);
 }
