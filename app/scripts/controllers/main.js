e81979de9160e04374f377bdec4b13492d328cb2
Lauriane Anthony <lauriane.anthony@talkspirit.com>
2015-03-24 14:38:39 +0100

    arrange design for page project detail; add edit function for title and description

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -19,17 +19,26 @@ angular.module('clapItApp')
 angular.module('clapItApp')
   .controller('ProjectCtrl', function ($scope, localStorageService) {
     
-    $scope.projects = getLocalStorage('projects', localStorageService);
+    // Init projects with projects in localStorage
+    $scope.projects = getData('projects', localStorageService);
 
+    // Watch change of projects and automatically update in localStorage
     $scope.$watch('projects', function () {
-      saveLocalStorage('projects', $scope.projects, localStorageService);
+      saveData('projects', $scope.projects, localStorageService);
     }, true);
 
+    // Option for ui-sortable : add button for draggable option
+    $scope.sortableOptions = {
+      handle: '.itemProject .toolBar',
+    };
+
+    // Add new project
     $scope.addProject = function() {
       $scope.projects.push($scope.project);
       $scope.project=('');
     };
 
+    // Remove a project
     $scope.removeProject= function(index) {
       $scope.projects.splice(index, 1);
     };
@@ -40,67 +49,97 @@ angular.module('clapItApp')
   .controller('ProjectDetailCtrl', ['$scope', '$routeParams', 'localStorageService', '$location',
   function($scope, $routeParams, localStorageService, $location) {
 
-    var projects = getLocalStorage('projects', localStorageService);
-
-    // Show project by id
-    var projectId = $routeParams.projectId;
-    $scope.projectId = projectId.split(':')[1];
-
-    $scope.project= projects[$scope.projectId];
+    // Init projects with projects in localStorage
+    $scope.projects = getData('projects', localStorageService);
 
     $scope.$watch('projects', function () {
-      saveLocalStorage('projects', projects, localStorageService);
+      saveData('projects', $scope.projects, localStorageService);
     }, true);
 
+
+    // Find detail for the current project (by id)
+    var projectId = $routeParams.projectId;
+    $scope.projectId = projectId.split(':')[1];
+    $scope.project= $scope.projects[$scope.projectId];
+
+    // Remove a project and redirect to project page
     $scope.removeProject= function(index) {
-      projects.splice(index, 1);
-      //console.log(projects);
-      saveLocalStorage('projects', projects, localStorageService);
+      $scope.projects.splice(index, 1);
       $location.path('/project');
     };
+
     
-
-    $scope.personArray = [{
-      name: '',
-      fonction: ''
-    }];
-    $scope.addNewLines = function() {
-      $scope.personArray.push({name: '', fonction: ''});
-    };
-
-    $scope.lists = [
-      { name: 'Equipe technique',value: 'technique'},
-      { name: 'Comédiens', value: 'actor'},
-      { name: 'Nouvelle liste', value: 'new'},
-    ];
-
-
-    $scope.addList = function() {
-      var name = $scope.project.name;
-      var description = $scope.project.description;
-
-      $scope.project = {
-        name : name,
-        description : description,
-        listComedien : {
-          listName: $scope.list,
-          list: $scope.personArray
-        }
+    /******* Add List ******/
+      // Structure of input for person in lists
+      $scope.personArray = [{
+        name: '',
+        fonction: ''
+      }];
+      $scope.addNewLines = function() {
+        $scope.personArray.push({name: '', fonction: ''});
       };
-      angular.forEach(projects, function(value, key){
-        if (key == $scope.projectId)  {
-          projects[key]= $scope.project;
+
+      // Structure of select list
+      $scope.lists = [
+        { name: 'Equipe technique',value: 'technique'},
+        { name: 'Comédiens', value: 'actor'},
+        { name: 'Administratif', value: 'admin'},
+      ];
+
+      // Save list
+      $scope.addList = function() {
+        var name = $scope.project.name;
+        var description = $scope.project.description;
+        var listTech = $scope.project.listTechnique;
+        var listAct = $scope.project.listComedien;
+        var listAdmin = $scope.project.listAdministration;
+
+        switch($scope.list){
+          case 'technique':
+            $scope.project = {
+              name : name,
+              description : description,
+              listComedien : listAct,
+              listTechnique : {
+                listName: 'Equipe technique',
+                list: $scope.personArray
+              },
+              listAdministration : listAdmin
+            };
+          break;
+          case 'actor':
+            $scope.project = {
+              name : name,
+              description : description,
+              listComedien : {
+                listName: 'Comédiens',
+                list: $scope.personArray
+              },
+              listTechnique : listTech,
+              listAdministration : listAdmin
+            };
+          break;
+          case 'admin':
+            $scope.project = {
+              name : name,
+              description : description,
+              listComedien : listAct,
+              listTechnique : listTech,
+              listAdministration : {
+                listName: 'Administratif',
+                list: $scope.personArray
+              }
+            };
+          break;
         }
-      });
-      console.log(projects);
+        
+        $scope.projects[$scope.projectId]=$scope.project;
 
-
-      localStorage.clear();
-      localStorageService.set('projects', projects);
-
+        $scope.personArray=[];
+        $('#addListForm').addClass('ng-hide');
+      };
 
       
-    };
 
 
 
